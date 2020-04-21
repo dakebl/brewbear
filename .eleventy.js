38df381e94e3cf8e96fd7e5e54fece09ebdfc580
Dale Blackburn <dale@dakebl.co.uk>
2020-04-21 15:00:06 +0100

    :building_construction: Renamed dist directory to website

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,33 @@
 // docs: https://www.11ty.io/docs/config/
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
-    
     callbacks: {
-     // add a 404 page
-    /*   ready: function(err, bs) {
-         const content_404 = fs.readFileSync('_site/404.html');
-         bs.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-       }, */
-    ghostMode: {
-      clicks: false,
-      forms: false,
-      scroll: false,
-    },
+      // add a 404 page
+      // ready: function (err, bs) {
+      //   const content_404 = fs.readFileSync('website/404.html');
+      //   bs.addMiddleware('*', (req, res) => {
+      //     // Provides the 404 content without redirect.
+      //     res.write(content_404);
+      //     res.end();
+      //   });
+      // },
+      ghostMode: {
+        clicks: false,
+        forms: false,
+        scroll: false,
+      },
     }
   });
 
   eleventyConfig.addPassthroughCopy('src/styles.css');
-  
+
   return {
     dir: {
-      input: "src",
-      output: "dist"
+      input: 'src',
+      output: 'website',
     },
     passthroughFileCopy: true
   };
